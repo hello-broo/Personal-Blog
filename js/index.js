20117e262b1f0eb3afc84f17dadb33a9899fac5e
Dhruv-958 <113263880+Dhruv-958@users.noreply.github.com>
2023-04-24 19:46:51 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,7 @@
+var typed2 = new Typed('#typed2', {
+    strings: ['Some <i>strings</i> with', 'Some <strong>HTML</strong>', 'Chars &times; &copy;'],
+    typeSpeed: 0,
+    backSpeed: 0,
+    fadeOut: true,
+    loop: true
+  });
